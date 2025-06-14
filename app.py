16e0d92f743078b093572cc6b651d747e41ebbbf
Denny <den.marx@web.de>
2025-06-15 00:06:44 +0200

    update route added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 app = Flask(__name__)
 @app.route("/")
 def index():
+    """main page"""
     with open("blog_posts.json", "r", encoding="utf-8") as file:
         blog_posts = json.load(file)
     return render_template("index.html", posts=blog_posts)
@@ -10,6 +11,7 @@ def index():
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
+    """add a post"""
     if request.method == "POST":
         title = request.form["title"]
         author = request.form["author"]
@@ -37,6 +39,7 @@ def add():
 
 @app.route("/delete/<int:post_id>")
 def delete(post_id):
+    """deletes a post"""
     with open("blog_posts.json", "r", encoding="utf-8") as file:
         blog_posts = json.load(file)
 
@@ -46,6 +49,30 @@ def delete(post_id):
         json.dump(blog_posts, file,indent=4)
 
     return redirect(url_for("index"))
+
+
+@app.route("/update/<int:post_id>", methods=["GET", "POST"])
+def update(post_id):
+    """updates a post"""
+    with open("blog_posts.json", "r", encoding="utf-8") as file:
+        blog_posts = json.load(file)
+
+    post_index = next((i for i, post in enumerate(blog_posts) if post["id"] == post_id), None)
+
+    if post_index is None:
+        return "post not found", 404
+
+    if request.method == "POST":
+        blog_posts[post_index]["title"] = request.form["title"]
+        blog_posts[post_index]["author"] = request.form["author"]
+        blog_posts[post_index]["content"] = request.form["content"]
+
+        with open("blog_posts.json", "w", encoding="utf-8") as file:
+            json.dump(blog_posts, file,indent=4)
+
+        return redirect(url_for("index"))
+
+    return render_template("update.html",post=blog_posts[post_index])
 
 
 if __name__ == '__main__':
