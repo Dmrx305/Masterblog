15a49ba88514b3b394eeae32a4824f44861739f9
Denny <den.marx@web.de>
2025-06-14 13:37:09 +0200

    json,html template and style added

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import json
 app = Flask(__name__)
-
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    with open("blog_posts.json", "r", encoding="utf-8") as file:
+        blog_posts = json.load(file)
 
+    return render_template("index.html", posts=blog_posts)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
